4acdef0c62493f590bbde3d57c99e683465c37c5
m7nter <gz796wvgy5@privaterelay.appleid.com>
2026-06-21 10:53:33 +0300

    Create CameraViewModel.swift

---
+++ Sources/SecureVault/Camera/CameraViewModel.swift
@@ -0,0 +1,59 @@
+// CameraViewModel.swift
+import AVFoundation
+import UIKit
+import Combine
+
+final class CameraViewModel: NSObject, ObservableObject {
+    @Published var capturedImage: UIImage?
+    @Published var error: String?
+
+    let session = AVCaptureSession()
+    private var photoOutput = AVCapturePhotoOutput()
+    private var completion: ((UIImage) -> Void)?
+
+    override init() {
+        super.init()
+        setupSession()
+    }
+
+    private func setupSession() {
+        session.beginConfiguration()
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                   for: .video, position: .back),
+              let input = try? AVCaptureDeviceInput(device: device) else {
+            error = "Camera unavailable"
+            return
+        }
+        if session.canAddInput(input) { session.addInput(input) }
+        if session.canAddOutput(photoOutput) { session.addOutput(photoOutput) }
+        session.commitConfiguration()
+    }
+
+    func startSession() {
+        if !session.isRunning { session.startRunning() }
+    }
+
+    func stopSession() {
+        if session.isRunning { session.stopRunning() }
+    }
+
+    func capturePhoto(completion: @escaping (UIImage) -> Void) {
+        self.completion = completion
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+}
+
+extension CameraViewModel: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput,
+                     didFinishProcessingPhoto photo: AVCapturePhoto,
+                     error: Error?) {
+        guard let data = photo.fileDataRepresentation(),
+              let img = UIImage(data: data) else { return }
+
+        let loc = LocationManager.shared.location
+        let hdg = LocationManager.shared.heading
+        let watermarked = WatermarkRenderer.apply(to: img, location: loc, heading: hdg)
+        DispatchQueue.main.async { self.completion?(watermarked) }
+    }
+}
