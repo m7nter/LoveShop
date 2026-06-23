11b1ab3a26746d8708c6dc71482072155086e040
m7nter <gz796wvgy5@privaterelay.appleid.com>
2026-06-23 14:10:10 +0300

    Update PhotoDetailView.swift

--- Sources/SecureVault/Vault/PhotoDetailView.swift
+++ Sources/SecureVault/Vault/PhotoDetailView.swift
@@ -4,11 +4,13 @@ struct PhotoDetailView: View {
     let url: URL
     let onDelete: () -> Void
     @Environment(\.dismiss) var dismiss
+    @State private var showEditor = false
+    @State private var currentImage: UIImage?
 
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-            if let data = try? Data(contentsOf: url), let img = UIImage(data: data) {
+            if let img = currentImage ?? loadImage() {
                 Image(uiImage: img)
                     .resizable()
                     .scaledToFit()
@@ -23,15 +25,31 @@ struct PhotoDetailView: View {
                         .background(Color.black.opacity(0.5))
                         .cornerRadius(8)
                     Spacer()
-                    Button {
-                        onDelete()
-                        dismiss()
-                    } label: {
-                        Image(systemName: "trash")
-                            .foregroundColor(.red)
-                            .padding(10)
-                            .background(Color.black.opacity(0.5))
-                            .clipShape(Circle())
+                    HStack(spacing: 12) {
+                        // Кнопка редактирования
+                        Button {
+                            if let img = currentImage ?? loadImage() {
+                                currentImage = img
+                                showEditor = true
+                            }
+                        } label: {
+                            Image(systemName: "pencil")
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .background(Color.black.opacity(0.5))
+                                .clipShape(Circle())
+                        }
+
+                        Button {
+                            onDelete()
+                            dismiss()
+                        } label: {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                                .padding(10)
+                                .background(Color.black.opacity(0.5))
+                                .clipShape(Circle())
+                        }
                     }
                 }
                 .padding(.horizontal, 16)
@@ -39,5 +57,27 @@ struct PhotoDetailView: View {
                 Spacer()
             }
         }
+        .onAppear {
+            currentImage = loadImage()
+        }
+        .fullScreenCover(isPresented: $showEditor) {
+            if let img = currentImage {
+                PhotoEditorView(image: img) { edited in
+                    // Перезаписываем файл
+                    if let data = edited.jpegData(compressionQuality: 0.95) {
+                        try? data.write(to: url)
+                    }
+                    currentImage = edited
+                    showEditor = false
+                } onDiscard: {
+                    showEditor = false
+                }
+            }
+        }
+    }
+
+    private func loadImage() -> UIImage? {
+        guard let data = try? Data(contentsOf: url) else { return nil }
+        return UIImage(data: data)
     }
 }
