b5c246a464e517197ef4dbc2661326a8b361c439
m7nter <gz796wvgy5@privaterelay.appleid.com>
2026-06-24 11:49:33 +0300

    Update CrosshairView.swift

--- Sources/SecureVault/Camera/CrosshairView.swift
+++ Sources/SecureVault/Camera/CrosshairView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct CrosshairView: View {
     let color: String
 
-    private var uiColor: Color {
+    private var lineColor: Color {
         switch color {
         case "red": return .red
         case "green": return .green
@@ -14,53 +14,16 @@ struct CrosshairView: View {
 
     var body: some View {
         ZStack {
-            // Горизонтальная линия
+            // Горизонтальная линия на весь экран
             Rectangle()
-                .fill(uiColor.opacity(0.8))
-                .frame(width: 60, height: 1)
+                .fill(lineColor)
+                .frame(height: 2)
 
-            // Вертикальная линия
+            // Вертикальная линия на весь экран
             Rectangle()
-                .fill(uiColor.opacity(0.8))
-                .frame(width: 1, height: 60)
-
-            // Центральная точка
-            Circle()
-                .fill(uiColor.opacity(0.8))
-                .frame(width: 4, height: 4)
-
-            // Угловые метки
-            CrosshairCorners(color: uiColor)
+                .fill(lineColor)
+                .frame(width: 2)
         }
-    }
-}
-
-struct CrosshairCorners: View {
-    let color: Color
-
-    var body: some View {
-        ZStack {
-            // Верхний левый
-            corner().offset(x: -15, y: -15).rotationEffect(.degrees(0))
-            // Верхний правый
-            corner().offset(x: 15, y: -15).rotationEffect(.degrees(90))
-            // Нижний правый
-            corner().offset(x: 15, y: 15).rotationEffect(.degrees(180))
-            // Нижний левый
-            corner().offset(x: -15, y: 15).rotationEffect(.degrees(270))
-        }
-    }
-
-    private func corner() -> some View {
-        ZStack {
-            Rectangle()
-                .fill(color.opacity(0.8))
-                .frame(width: 10, height: 1)
-                .offset(x: 5, y: 0)
-            Rectangle()
-                .fill(color.opacity(0.8))
-                .frame(width: 1, height: 10)
-                .offset(x: 0, y: 5)
-        }
+        .ignoresSafeArea()
     }
 }
